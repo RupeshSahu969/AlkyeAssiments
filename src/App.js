b8cdac431ca27eb35cb78b0a8978a250cc0e0892
Rupesh Sahu <rupeshsahu969@gmail.com>
2024-07-05 14:57:36 +0530

    add this signup1 and footer pages responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Signup1 from './Components/Signup1';
+import Footer from './Pages/Footer';
 
 function App() {
   return (
     <div className="App">
    <Signup1/>
+   {/* <Footer/> */}
     </div>
   );
 }
